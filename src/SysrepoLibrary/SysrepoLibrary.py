4836bc6a75fd155d475204d531315ec64e5a58e1
agardijan <andrej.gardijan@sartura.hr>
2023-02-01 11:13:51 +0100

    remove trailing spaces

--- src/SysrepoLibrary/SysrepoLibrary.py
+++ src/SysrepoLibrary/SysrepoLibrary.py
@@ -151,9 +151,9 @@ class SysrepoLibrary(object):
             raise RuntimeError(f"Non-existing session index {sessID}")
 
         with self.conns[connID].get_ly_ctx() as ctx:
-            yangData = ctx.parse_data_mem(data, 
-                                          fmt, 
-                                          no_state=True, 
+            yangData = ctx.parse_data_mem(data,
+                                          fmt,
+                                          no_state=True,
                                           strict=True)
 
         self.sessions[connID][sessID].edit_batch_ly(yangData)
